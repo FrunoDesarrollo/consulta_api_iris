151d6c7924dd8b52e92a126f2860c80ed4e71ad3
Jei <@>
2024-10-01 17:12:53 -0600

    Se actualiza función pretty_print para aceptar valores NULL.

--- shared/iniciar_clase.php
+++ shared/iniciar_clase.php
@@ -31,48 +31,56 @@ $api_iris = new ConsultarIris($configuracion_iris["iris_api"], $configuracion_ir
 // La siguiente función es para la demostración.
 function pretty_print(mixed $value, bool $is_child = false): void
 {
-    if (is_bool($value)) {
-        echo (true === $value) ? 'true' : 'false';
-        return;
+    $main_tipo_val = gettype($value);
+
+    if ("boolean" == $main_tipo_val) {
+        $value = (true === $value) ? 'true' : 'false';
+    }
+    if ("NULL" == $main_tipo_val) {
+        $value = 'NULL';
     }
 
-    if (is_string($value)) {
+    if (in_array($main_tipo_val, ["string", "boolean", "NULL"])) {
         echo '<head><meta charset="utf-8"><style>body,html,table{font-family:system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"}table{font-size:15px;margin:0;padding:0;border-spacing:0;border-collapse:collapse;background:#fff}td,th{border:1px solid #ddd;text-align:left;padding:8px}table tr:nth-child(even){background:#e8e8e8}</style></head><table><tbody><tr><th>'
             . $value . '</th></tbody></table>';
 
         return;
     }
 
-    if (is_object($value)) {
-        $value = (array)$value;
-    }
-    if (is_array($value)) {
+    if ("array" == $main_tipo_val || "object" == $main_tipo_val) {
         $even = "";
-        if (false === $is_child) {
+        if (!$is_child) {
             echo '<head><meta charset="utf-8"><style>body,html,table{font-family:system-ui,-apple-system,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans","Liberation Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"}table{font-size:15px;margin:0;padding:0;border-spacing:0;border-collapse:collapse;background:#fff}td,th{border:1px solid #ddd;text-align:left;padding:8px}table.is_child tr:nth-child(even){background:#e7e7e7}</style></head>';
         } else {
             $even = 'class="is_child"';
         }
         echo "<table $even><tbody>";
 
+        if ("object" == $main_tipo_val) {
+            $value = (array)$value;
+        }
+
         foreach ($value as $key => $val) {
 
             if (is_string($key) || is_numeric($key)) {
-                echo "<tr><th>$key</th><td>";
+                echo "<tr><th>";
+                echo $key;
+                echo "</th><td>";
             } else {
                 echo "<tr><td>";
             }
 
-            if (is_bool($val)) {
-                $val = (true === $val ? 'true' : 'false');
-            } elseif (is_object($val)) {
-                $val = (array)$val;
-            }
+            $tipo_val = gettype($val);
 
-            if (is_array($val)) {
-                pretty_print($val, true);
+            if ("array" == $tipo_val || "object" == $tipo_val) {
+                pretty_print((array)$val, true);
             } else {
-                echo $val;
+                echo match ($tipo_val) {
+                    "NULL" => 'NULL',
+                    "integer", "double", "string" => $val,
+                    "boolean" => (true === $val) ? 'true' : 'false',
+                    default => var_export($val, true),
+                };
             }
 
             echo '</td></tr>';
